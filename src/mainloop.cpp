ace41ef24fa0735c124b25d1cbf80b38743912ed
José Roberto de Souza <jose.souza@intel.com>
2017-04-12 15:52:25 -0700

    Move part of main code to a mainloop class

--- src/mainloop.cpp
+++ src/mainloop.cpp
@@ -51,12 +51,8 @@
 
 using namespace cv;
 
-#if DEBUG_LEVEL
-const char *window_name = "Aero down face camera test";
-#endif
-
 // OV7251 only supports this resolutions each with a different FPS
-struct {
+static struct {
 	uint32_t width;
 	uint32_t height;
 } resolutions[] = {
@@ -64,6 +60,7 @@ struct {
 		{320, 240},
 		{160, 120},
 };
+
 #define DEFAULT_RESOLUTION 1
 #define DEFAULT_IMG_WIDTH resolutions[DEFAULT_RESOLUTION].width
 #define DEFAULT_IMG_HEIGHT resolutions[DEFAULT_RESOLUTION].height
@@ -71,23 +68,43 @@ struct {
 #define DEFAULT_PIXEL_FORMAT V4L2_PIX_FMT_YUV420
 #define DEFAULT_DEVICE_ID 1
 
-static struct gyro_data_t {
-	float x, y, z;
-	uint64_t time_usec;
-} gyro_data;
-static bool ready_to_send_msg;
-static Mavlink_UDP *mavlink;
+class Mainloop {
+public:
+	int run();
 
-static const char *default_device = "/dev/video2";
+	void camera_callback(const void *img, size_t len, struct timeval *timestamp);
+	void highres_imu_msg_callback(const mavlink_highres_imu_t *msg);
 
-static bool should_run;
+private:
+
+#if DEBUG_LEVEL
+	const char *_window_name = "Aero down face camera test";
+#endif
+	const char *default_device = "/dev/video2";
+
+	struct gyro_data_t {
+		float x, y, z;
+		uint64_t time_usec;
+	} _gyro_data;
+
+	bool _ready_to_send_msg = false;
+
+	Camera *_camera;
+	OpticalFlowOpenCV *_optical_flow;
+	Mavlink_UDP *_mavlink;
+
+	void signal_handlers_setup();
+	void loop();
+};
+
+static bool _should_run;
 
 static void exit_signal_handler(int signum)
 {
-    should_run = false;
+    _should_run = false;
 }
 
-static void signal_handlers_setup(void)
+void Mainloop::signal_handlers_setup(void)
 {
     struct sigaction sa = { };
 
@@ -100,12 +117,14 @@ static void signal_handlers_setup(void)
     sigaction(SIGPIPE, &sa, NULL);
 }
 
-static void loop(Pollable *pollables[], uint8_t len)
+void Mainloop::loop()
 {
+	Pollable *pollables[] = { _camera, _mavlink };
+	const uint8_t len = sizeof(pollables) / sizeof(Pollable *);
 	struct pollfd desc[len];
 
 	signal_handlers_setup();
-	should_run = true;
+	_should_run = true;
 
 	for (uint8_t i = 0; i < len; i++) {
 		desc[i].fd = pollables[i]->_fd;
@@ -113,7 +132,7 @@ static void loop(Pollable *pollables[], uint8_t len)
 		desc[i].revents = 0;
 	}
 
-	while (should_run) {
+	while (_should_run) {
 		int ret = poll(desc, sizeof(desc) / sizeof(struct pollfd), -1);
 		if (ret < 1) {
 			continue;
@@ -135,25 +154,30 @@ static void loop(Pollable *pollables[], uint8_t len)
 	}
 }
 
-static void camera_callback(const void *img, size_t len, struct timeval *timestamp, void *data)
+static void _camera_callback(const void *img, size_t len, struct timeval *timestamp, void *data)
 {
-	int dt_us;
+	Mainloop *mainloop = (Mainloop *)data;
+	mainloop->camera_callback(img, len, timestamp);
+}
+
+void Mainloop::camera_callback(const void *img, size_t len, struct timeval *timestamp)
+{
+	int dt_us = 0;
 	float x = 0, y = 0;
-	OpticalFlowOpenCV *optical_flow = (OpticalFlowOpenCV *)data;
 
 	Mat frame_gray = Mat(DEFAULT_IMG_HEIGHT, DEFAULT_IMG_WIDTH, CV_8UC1);
 	frame_gray.data = (uchar*)img;
 
 #if DEBUG_LEVEL
-	imshow(window_name, frame_gray);
+	imshow(_window_name, frame_gray);
 #endif
 
 	DEBUG("camera callback timestamp: sec=%lu usec=%lu", timestamp->tv_sec, timestamp->tv_usec);
 
-	int quality = optical_flow->calcFlow(frame_gray.data, timestamp->tv_sec, dt_us, x, y);
+	int quality = _optical_flow->calcFlow(frame_gray.data, timestamp->tv_sec, dt_us, x, y);
 	DEBUG("Optical flow data: quality=%i x=%f y=%f dt_us=%i", quality, x, y, dt_us);
 
-	if (!ready_to_send_msg) {
+	if (!_ready_to_send_msg) {
 		DEBUG("Not ready to send optical flow message");
 		return;
 	}
@@ -163,92 +187,99 @@ static void camera_callback(const void *img, size_t len, struct timeval *timesta
 	msg.integration_time_us = dt_us;
 	msg.integrated_x = x;
 	msg.integrated_y = y;
-	msg.integrated_xgyro = gyro_data.x;
-	msg.integrated_ygyro = gyro_data.y;
-	msg.integrated_zgyro = gyro_data.z;
+	msg.integrated_xgyro = _gyro_data.x;
+	msg.integrated_ygyro = _gyro_data.y;
+	msg.integrated_zgyro = _gyro_data.z;
 	msg.time_delta_distance_us = 0;
 	msg.distance = -1.0;
 	msg.temperature = 0;
 	msg.sensor_id = 0;
 	msg.quality = quality;
 
-	mavlink->optical_flow_rad_msg_write(&msg);
+	_mavlink->optical_flow_rad_msg_write(&msg);
 }
 
-static void highres_imu_msg_subscribe(const mavlink_highres_imu_t *msg, void *data)
+static void _highres_imu_msg_callback(const mavlink_highres_imu_t *msg, void *data)
 {
-	gyro_data.x = msg->xgyro;
-	gyro_data.y = msg->ygyro;
-	gyro_data.z = msg->zgyro;
-	gyro_data.time_usec = msg->time_usec;
-	ready_to_send_msg = true;//TODO check liveness
+	Mainloop *mainloop = (Mainloop *)data;
+	mainloop->highres_imu_msg_callback(msg);
 }
 
-int main()
+void Mainloop::highres_imu_msg_callback(const mavlink_highres_imu_t *msg)
 {
-	Camera *camera;
-	OpticalFlowOpenCV *optical_flow;
-	Pollable *pollables[2];
+	_gyro_data.x = msg->xgyro;
+	_gyro_data.y = msg->ygyro;
+	_gyro_data.z = msg->zgyro;
+	_gyro_data.time_usec = msg->time_usec;
+	_ready_to_send_msg = true;//TODO check liveness
+}
+
+int Mainloop::run()
+{
 	int ret;
 
-	camera = new Camera(default_device);
-	if (!camera) {
+	_camera = new Camera(default_device);
+	if (!_camera) {
 		ERROR("No memory to instantiate Camera");
 		return -1;
 	}
-	ret = camera->init(DEFAULT_DEVICE_ID, DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT, DEFAULT_PIXEL_FORMAT);
+	ret = _camera->init(DEFAULT_DEVICE_ID, DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT, DEFAULT_PIXEL_FORMAT);
 	if (ret) {
 		ERROR("Unable to initialize camera");
 		goto camera_init_error;
 	}
 
-	mavlink = new Mavlink_UDP();
-	if (!mavlink) {
+	_mavlink = new Mavlink_UDP();
+	if (!_mavlink) {
 		ERROR("No memory to instantiate Mavlink_UDP");
 		goto mavlink_memory_error;
 	}
-	ret = mavlink->init("127.0.0.1", 14555);
+	ret = _mavlink->init("127.0.0.1", 14555);
 	if (ret) {
 		ERROR("Unable to initialize mavlink");
 		goto mavlink_init_error;
 	}
-	mavlink->highres_imu_msg_subscribe(highres_imu_msg_subscribe, NULL);
+	_mavlink->highres_imu_msg_subscribe(_highres_imu_msg_callback, this);
 
 	// TODO: get the real value of f_length_x and f_length_y
 	// TODO:  check that image format it uses
-	optical_flow = new OpticalFlowOpenCV(1, 1, -1, DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT);
-	if (!optical_flow) {
+	_optical_flow = new OpticalFlowOpenCV(1, 1, -1, DEFAULT_IMG_WIDTH, DEFAULT_IMG_HEIGHT);
+	if (!_optical_flow) {
 		ERROR("No memory to instantiate OpticalFlowOpenCV");
 		goto optical_memory_error;
 	}
-	camera->callback_set(camera_callback, optical_flow);
+	_camera->callback_set(_camera_callback, this);
 
 #if DEBUG_LEVEL
-	namedWindow(window_name, WINDOW_AUTOSIZE);
+	namedWindow(_window_name, WINDOW_AUTOSIZE);
 	startWindowThread();
 #endif
 
-	pollables[0] = camera;
-	pollables[1] = mavlink;
-	loop(pollables, 2);
+	loop();
 
 #if DEBUG_LEVEL
 	destroyAllWindows();
 #endif
 
-	delete optical_flow;
-	delete mavlink;
-	camera->shutdown();
-	delete camera;
+	delete _optical_flow;
+	delete _mavlink;
+	_camera->shutdown();
+	delete _camera;
 
 	return 0;
 
 optical_memory_error:
 mavlink_init_error:
-	delete mavlink;
+	delete _mavlink;
 mavlink_memory_error:
-	camera->shutdown();
+	_camera->shutdown();
 camera_init_error:
-	delete camera;
+	delete _camera;
 	return -1;
+}
+
+int main()
+{
+	Mainloop mainloop;
+	return mainloop.run();
 }
